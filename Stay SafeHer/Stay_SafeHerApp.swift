e99e1f7f66d00a5e8030c7cfb9af0e8e09ebc5c0
Maya <mayaputerman@gmail.com>
2023-06-26 14:12:19 -0400

    Initial Commit

---
+++ Stay SafeHer/Stay_SafeHerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stay_SafeHerApp.swift
+//  Stay SafeHer
+//
+//  Created by Maya Puterman on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Stay_SafeHerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
