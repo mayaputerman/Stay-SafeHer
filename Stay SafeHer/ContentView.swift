6ef1b7943079b5862d5620d58b47f26190ceed91
Maya <mayaputerman@gmail.com>
2023-06-29 13:00:12 -0400

    i am testing this first commit

--- Stay SafeHer/ContentView.swift
+++ Stay SafeHer/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("hey it's maya!")
         }
         .padding()
     }
